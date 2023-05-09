b0f880c44289e625d355d1fbd69f567230d2a5b6
Prince Ndukwe <princendu54@gmail.com>
2023-05-09 16:22:26 +0100

    API Photos added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react'
 import Car from './components/Car'
 import Fetch from './components/Fetch'
 import FetchUsers from './components/FetchUsers'
+import FetchPhoto from './components/FetchPhoto'
+import  './App.css'
 
 const App = () => {
   return (
     <div>
-      <Car brand='Toyota' color = 'Red'/>
+      {/* <Car brand='Toyota' color = 'Red'/>
       <Car brand='Lexus' color = 'Black'/>
       <Car brand='Mercedes' color = 'Green'/>
 
       <Fetch />
-      <FetchUsers />
+      <FetchUsers /> */}
+
+      <div className='image'>
+      <FetchPhoto />
+      </div>
     </div>
   )
 }
---
+++ src/components/FetchPhoto.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+
+const FetchPhoto = () => {
+    const [photo, setPhoto] = useState([]);
+    useEffect ( () => {
+            fetch("https://jsonplaceholder.typicode.com/photos")
+            .then(response=> response.json())
+            .then(response => setPhoto(response))
+            .catch(error => console.log(error))
+    }, [])
+  return (
+    <div className='d-flex flex-wrap gap-3'>
+{
+    photo.map((photo, index)=>(
+<div class="card" style={{width:"18rem"}} key={index}>
+<p class="card-text">{photo.id} </p>
+  <img src={photo.url} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">{photo.title}</h5>
+    
+    <a href="#">{photo.thumbnailUrl}</a>
+  </div>
+</div>
+
+    ))
+
+
+}
+    </div>
+  )
+}
+
+export default FetchPhoto
